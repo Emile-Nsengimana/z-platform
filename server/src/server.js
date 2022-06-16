6113a23f04dcb51183222f42e920a7142a50adfc
Emile-Nsengimana <emile.nsengimana@codeland.it>
2022-06-16 14:51:26 +0200

    server setup

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+import 'regenerator-runtime';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+ 
+ 
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Welcome to ZPlatform APIs');
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${PORT}`);
+});
+
+export default app;
